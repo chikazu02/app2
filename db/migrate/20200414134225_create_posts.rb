5a9b3d988f5d4eb3db5df1b5a6b1f00560ad1b1b
chikazu02 <soraa.lastdrg2@gmail.com>
2020-04-16 23:09:32 +0900

    migrate

--- db/migrate/20200414134225_create_posts.rb
+++ db/migrate/20200414134225_create_posts.rb
@@ -1,7 +1,11 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
-      
+      t.string :for 
+      t.integer :train_number
+      t.integer :car_number
+      t.string :seat_number
+      t.string :type
       t.timestamps
     end
   end
